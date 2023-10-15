c13d287619305c15313885c6c78d352bd41158b5
ahmet <adsayahmet94@hotmail.com>
2023-10-15 17:11:44 +0300

    added login page

--- app/login.js
+++ app/login.js
@@ -1,13 +1,115 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  StyleSheet,
+} from "react-native";
 
-const login = () => {
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    // handle login logic here
+  };
+
+  const handleSignup = () => {
+    // handle signup logic here
+  };
+
+  const getRandomColor = () => {
+    // Generate a random color in the format #RRGGBB
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+
+  const applyRandomColor = (text) => {
+    // Apply random color to each letter of the text
+    return text.split("").map((letter, index) => (
+      <Text key={index} style={{ color: getRandomColor() }}>
+        {letter}
+      </Text>
+    ));
+  };
+
   return (
-    <View>
-      <Text>login</Text>
+    <View style={styles.container}>
+      <Image source={require("../assets/book.png")} style={styles.image} />
+      <Text style={styles.title}>
+        {applyRandomColor("Welcome to BookLove")}
+      </Text>
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          value={email}
+          onChangeText={setEmail}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+        <TouchableOpacity style={styles.button} onPress={handleLogin}>
+          <Text style={styles.buttonText}>Log In</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={handleSignup}>
+          <Text style={styles.buttonText}>Sign Up</Text>
+        </TouchableOpacity>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default login
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#FFFDD0",
+  },
+  image: {
+    width: 300,
+    height: 300,
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+    flexDirection: "row",
+  },
+  form: {
+    width: "80%",
+    gap: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: "#0b5da1",
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: "#fff",
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+});
 
+export default Login;
