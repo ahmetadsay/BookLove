640171c8a20c45119a53774ebc71cb1e3cd7318d
ahmet <adsayahmet94@hotmail.com>
2023-11-01 18:40:09 +0300

    added book content

--- app/readBook/[id].js
+++ app/readBook/[id].js
@@ -1,32 +1,28 @@
- // BookReader.js
-import React, { useEffect, useState } from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import axios from 'axios';
-import HTML from 'react-native-render-html';
-import { usePathname } from 'expo-router';
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { WebView } from 'react-native-webview';
 
-const BookReader = ({ bookId }) => {
-  const [bookContent, setBookContent] = useState('');
-  const pathname = usePathname();
-  const id = pathname.split("/")[2];
-
-  useEffect(() => {
-    // Fetch the e-book content from the Project Gutenberg catalog API
-    axios
-      .get(`https://www.gutenberg.org/ebooks/${id}.txt.utf-8`)
-      .then(response => {
-        setBookContent(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching book content: ', error);
-      });
-  }, [bookId]);
+const BookReader = () => {
+  // Replace with the actual URL of the book in a reader-friendly format (e.g., "text/plain" or "text/html")
+  const bookUrl = 'https://www.gutenberg.org/ebooks/1513.html.images';
 
   return (
-    <ScrollView>
-      <HTML source={{ html: bookContent }} />
-    </ScrollView>
+    <View style={styles.container}>
+      <WebView
+        source={{ uri: bookUrl }}
+        style={styles.webview}
+      />
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  webview: {
+    flex: 1,
+  },
+});
 
 export default BookReader;
