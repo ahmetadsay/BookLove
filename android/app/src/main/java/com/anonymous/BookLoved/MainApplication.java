f408d07c6aac51e6fa76d021cb3cda8aa5949561
ahmet <adsayahmet94@hotmail.com>
2024-02-14 18:57:33 +0300

    added npm run  android-package

--- android/app/src/main/java/com/anonymous/BookLoved/MainApplication.java
+++ android/app/src/main/java/com/anonymous/BookLoved/MainApplication.java
@@ -1,4 +1,4 @@
-package com.anonymous.BookLoved;
+package com.anonymous.bookloved;
 
 import android.app.Application;
 import android.content.res.Configuration;
--- firebase/firebase.js
+++ firebase/firebase.js
@@ -11,15 +11,14 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC94UzZOsEM68AIeWzk2-5MWofI0AQ1YBU",
+  apiKey: "AIzaSyBPUtDluhXbH9hGS1bV8Jq5tEXuxXlJ-gM",
   authDomain: "booklove-2a0b9.firebaseapp.com",
   projectId: "booklove-2a0b9",
   storageBucket: "booklove-2a0b9.appspot.com",
   messagingSenderId: "579880523945",
-  appId: "1:579880523945:android:79e6ff883edb24f6ebb1b8",
+  appId: "1:579880523945:web:1f234225f5790d52ebb1b8",
   measurementId: "G-HW9370PX5V"
 };
-
 // Initialize Firebase
 
 const app = initializeApp(firebaseConfig);
