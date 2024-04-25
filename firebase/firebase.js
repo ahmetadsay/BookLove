65ebc26fe443ebc6a0ccad708470e490a645262f
ahmet <adsayahmet94@hotmail.com>
2024-04-26 00:03:47 +0300

    21214 attemt

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,7 +1,6 @@
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -9,6 +8,9 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { getApps } from "firebase/app";
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBPUtDluhXbH9hGS1bV8Jq5tEXuxXlJ-gM",
   authDomain: "booklove-2a0b9.firebaseapp.com",
@@ -18,12 +20,11 @@ const firebaseConfig = {
   appId: "1:579880523945:web:1f234225f5790d52ebb1b8",
   measurementId: "G-HW9370PX5V"
 };
-// Initialize Firebase
+console.log(firebaseConfig);
 
-const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
 const db = getFirestore(app);
 const auth = getAuth(app);
-
-console.log("Firebase initialized");
 
 export { db, auth };
